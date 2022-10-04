c10b0f78d4338d561c6a65fd6d2d37d4dd1a35a5
August McAllister <august.chm@gmail.com>
2022-10-04 18:22:59 -0500

    Addressed JS stretch goal 9

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -74,3 +74,12 @@ console.log('8. Total supplies available is:', totalSupplies);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let looseParts = 572;
+let fullBoxes = 0;
+do {
+  looseParts -= 7;
+  fullBoxes += 1;
+} while (looseParts >= 7);
+
+console.log(`We have ${fullBoxes} full boxes of parts, and ${looseParts} parts left over.`);
